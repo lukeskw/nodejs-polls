6c66476244e39e432718bada13a068ba88389488
lukeskw <lucasporfirioa@gmail.com>
2024-02-08 15:02:36 -0300

    refactor: creating a better structure to routes

--- src/http/routes/polls/create-polls.routes.ts
+++ src/http/routes/polls/create-polls.routes.ts
@@ -3,20 +3,28 @@ import { ZodError, z } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 import { prisma } from '../../../lib/prisma';
 
-export const polls = async (app: FastifyInstance) => {
+export const createPolls = async (app: FastifyInstance) => {
 
   app.post('/polls', async (request, reply) => {
 
     const createPollBody = z.object({
-      title: z.string().min(5)
+      title: z.string().min(5),
+      options: z.array(z.string()).min(1)
     })
 
     try {
-      const { title } = createPollBody.parse(request.body)
+      const { title, options } = createPollBody.parse(request.body)
 
       const poll = await prisma.poll.create({
         data: {
           title,
+          options: {
+            createMany: {
+              data: options.map( option => {
+                return { title: option }
+              })
+            }
+          }
         }
       })
 
