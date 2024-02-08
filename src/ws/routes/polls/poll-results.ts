d998c8f0481cc4d5d7fab766ab9b7ffeb455da6c
lukeskw <lucasporfirioa@gmail.com>
2024-02-08 18:22:05 -0300

    ft: integrating the websocket to listen to the sub channel

---
+++ src/ws/routes/polls/poll-results.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import { voting } from "../../../utils/voting-pubsub";
+import z, { ZodError } from "zod";
+import { fromZodError } from "zod-validation-error";
+
+export const pollResults = async(app: FastifyInstance) => {
+  app.get('/polls/:pollId/results', { websocket: true }, ( connection, request ) => {
+    // pub/sub pattern
+    //subscribe only on published messages on the channel with pollId
+
+      const getPollParam = z.object({
+        pollId: z.string().uuid(),
+      })
+
+      try {
+
+        const { pollId } = getPollParam.parse(request.params)
+
+        voting.subscribe(pollId, (message) => {
+          connection.socket.send(JSON.stringify(message));
+        })
+
+      } catch( err ){
+        if(err instanceof ZodError){
+          const validationError = fromZodError(err);
+          console.error('Error parsing request:', validationError.message);
+          return connection.socket.send({ error: validationError.message });
+        }
+        console.error(String(err))
+        throw new Error(String(err))
+      }
+  })
+}
